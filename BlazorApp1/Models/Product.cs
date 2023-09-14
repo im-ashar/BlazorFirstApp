20ac7238fc3142f77285b5313c46a64f62053b7d
im-ashar <muhammadashar.401@gmail.com>
2023-09-14 10:58:52 +0500

    Add project files.

---
+++ BlazorApp1/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Price { get; set; }
+    }
+}
